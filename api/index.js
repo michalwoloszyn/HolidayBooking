11b2b8f43fd00218057bf0d1a51e4e0bdf5b5923
Michal Woloszyn <woloszyn.contact@gmail.com>
2023-06-23 12:09:24 +0200

    jwt change10

--- api/index.js
+++ api/index.js
@@ -68,7 +68,9 @@ app.post('/login', async (req, res) => {
         (err, token) => {
           if (err) throw err;
 
-          res.cookie('token', token).json(userDoc);
+          res
+            .cookie('token', token, { sameSite: 'none', secure: true })
+            .json(userDoc);
         }
       );
     } else {
